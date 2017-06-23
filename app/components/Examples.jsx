77403df167faf03463c2124559daeabb0b3d3a67
Ankhbayar <ankhbayarsb@gmail.com>
2017-06-23 12:05:44 +0800

    Rename example page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -3,9 +3,11 @@ var React = require('react');
 
 var Examples = (props) => {
   return(
-    <h3>Examples Component!</h3>
-    <p>Welcome to examples page!</p>
-  );
+    <div>
+      <h3>Examples</h3>
+      <p>Welcome to examples page!</p>
+    </div>
+  )
 };
 
 module.exports = Examples;
