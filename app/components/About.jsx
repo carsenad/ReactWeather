e59b43ded0e58219f6d3c3022a0ec6b6ac44513b
Ankhbayar <ankhbayarsb@gmail.com>
2017-06-24 11:47:49 +0800

    Add custom styles support

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,7 +3,7 @@ var React = require('react');
 var About = (props) => {
   return(
     <div>
-      <h1 className="text-center">About</h1>
+      <h1 className="text-center page-title">About</h1>
       <p>
         This is a weather application build on React. I have built this
         for The Complete React Web App Developer Course.
@@ -17,7 +17,7 @@ var About = (props) => {
             JavaScript framework used.
         </li>
         <li>
-          <a href="http://openweathermap.org">Open Weather Map</a> - I used 
+          <a href="http://openweathermap.org">Open Weather Map</a> - I used
             Open Weather Map to search for weather data by sity name.
         </li>
       </ul>
